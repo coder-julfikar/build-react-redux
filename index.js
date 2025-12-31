7a615ca1b85f1b69ba9f3e9be756de2155a4e1c6
coder-julfikar <julfikar_islam_khan@hotmail.com>
2025-12-31 21:19:19 +0600

    Multiple reducers are implemented with single reducer problems.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const redux = require('redux');
 console.log('---React-Redux/App---');
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAMS = 'BUY_ICECREAMS';
 
 // action creator|returns an action
 function buyCake(){
@@ -12,12 +13,44 @@ function buyCake(){
     }
 }
 
+function buyIceCreams(){
+    return {
+        type: BUY_ICECREAMS,
+        info: '2nd-redux-action'
+    }
+}
+
 // (prevState, action) => newState
 const initState = {
-    numberOfCakes: 10
+    numberOfCakes: 10,
+    numberOfIceCreams: 20,
+};
+
+// 4multipleReducer
+const initStateCake = {
+    numberOfCakes: 10,
+};
+const initStateIceCreams = {
+    numberOfIceCreams: 20,
 };
 
 const reducer = (state=initState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numberOfCakes: state.numberOfCakes-1
+        }
+
+        case BUY_ICECREAMS: return {
+            ...state,
+            numberOfIceCreams: state.numberOfIceCreams-1
+        }
+
+        default: return state
+    }
+};
+
+const cakeReducer = (state=initStateCake, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ...state,
@@ -28,12 +61,30 @@ const reducer = (state=initState, action) => {
     }
 };
 
-const store = redux.createStore(reducer); //redux.legacy_createStore(reducer)
+const icecreamReducer = (state=initStateIceCreams, action) => {
+    switch(action.type) {
+        case BUY_ICECREAMS: return {
+            ...state,
+            numberOfIceCreams: state.numberOfIceCreams-1
+        }
+        
+        default: return state
+    }
+};
+
+const combineReducers = redux.combineReducers;
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: icecreamReducer
+});
+const store = redux.createStore(rootReducer); //redux.legacy_createStore(reducer)
 console.log('Initial state => ', store.getState());
 const unsubscribe=store.subscribe(()=> console.log('Updated state => ', store.getState())); //anytime store updates, it's fired
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCreams());
+store.dispatch(buyIceCreams());
 unsubscribe();
 
 /*
